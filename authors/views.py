b8ea77cf6472fc8aeef8d6197ca89660670ac445
thiago alves <thiago87gsa@gmail.com>
2024-08-16 11:08:50 -0300

    :sparkles: feat: form part 1

---
+++ authors/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    return render(
+        request,
+        'authors/pages/register.html',
+        {'form': form}
+    )
