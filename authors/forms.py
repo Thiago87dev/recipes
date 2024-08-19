d04de0975193b00f37c4b7115afe82f86a8c9ec5
thiago alves <thiago87gsa@gmail.com>
2024-08-19 10:30:16 -0300

    :sparkles: feat: form part 2

--- authors/forms.py
+++ authors/forms.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(forms.ModelForm):
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +18,30 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # exclude = ['first_name']
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+                'max_length': 'This field must not have less then 5 chars',
+                'invalid': 'This field is invalid'
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+                'class': 'input text-input '
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here',
+            })
+        }
--- authors/views.py
+++ authors/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import Http404
 
 
 def register(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    request.session['number'] = request.session.get('number') or 0
+    request.session['number'] += 1
+    register_form_data = request.session.get('register_form_data')
+    form = RegisterForm(register_form_data)
     return render(
         request,
         'authors/pages/register.html',
         {'form': form}
     )
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
